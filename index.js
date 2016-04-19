77012397dad1a697d630d329fce73bafbe66d7b9
yuwancumian <yuwancumian@hotmail.com>
2016-04-19 15:12:21 +0800

    intinal commit

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+var bt3 = require('./bt3.json');
+var chalk = require('chalk');
+
+var queryName = process.argv[2];
+//console.log(item);
+
+for (name in bt3) {
+		// console.log(bt3[name]);
+		for (item in bt3[name]) {
+			//console.log(bt3[name][item]);
+			if (bt3[name][item] === queryName) {
+				console.log("<--")
+				console.log("The class was belonged to " + chalk.green(name) )
+				console.log("-->")
+			}
+		}
+}
