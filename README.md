bdcda5996950f6567092a0c59ab7dea33f001496
yuwancumian <easier2005@163.com>
2016-08-05 11:07:45 +0800

    add error tips

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-### Readme
+### bootstrap where
+
+一个命令行小工具 ，查询 bootstrap 某个具体的类处于哪个less 文件里
+
+```
+btw alert
+\\ This class was belong to alert.css
+
+btw something
+\\ Oops.. this class was not in!
+```
+
+
+
--- index.js
+++ index.js
@@ -3,16 +3,23 @@ var bt3 = require('./bt3.json');
 var chalk = require('chalk');
 
 var queryName = process.argv[2];
-//console.log(item);
 
-for (name in bt3) {
-	// console.log(bt3[name]);
-	for (item in bt3[name]) {
-		//console.log(bt3[name][item]);
-		if (bt3[name][item] === queryName) {
-			console.log("<--")
-			console.log("This class was belonged to " + chalk.green(name) )
-			console.log("-->")
+if (queryName === undefined){
+	console.log(chalk.gray("Pleae input class name!"))
+} else {
+	var check = false;
+	for (name in bt3) {
+		// console.log(bt3[name]);
+		for (item in bt3[name]) {
+			//console.log(bt3[name][item]);
+			if (bt3[name][item] === queryName) {
+				console.log("This class was belonged to " + chalk.green(name)+".");
+				var check = true;
+			}
+
 		}
+	}
+	if ( check === false ){
+		console.log(chalk.red("Oops.. this class was not in!"));
 	}
 }
